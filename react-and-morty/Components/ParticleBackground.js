1b2840dedc8f5a1437b9cb7864d666a18deb2100
RoxanaCristinaMarin <113606968+RoxanaCristinaMarin@users.noreply.github.com>
2023-05-29 03:37:02 +0300

    chore: added particle background

---
+++ react-and-morty/Components/ParticleBackground.js
@@ -0,0 +1,12 @@
+import "./ParticleBackground.scss";
+
+const ParticleBackground = () => {
+    return <div id="stars">
+                <div id="stars2"></div>
+                <div id="stars3"></div>
+                {/*<div className="particle"></div>*/}
+                {/*<div className="particle"></div>*/}
+            </div>
+}
+
+export default ParticleBackground;
